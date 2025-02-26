506460a486125ce664a0e44b86563df09039e521
Gupta <2392803@cognizant.com>
2025-02-26 16:49:40 +0530

    1st commit

---
+++ java1.java
@@ -0,0 +1,19 @@
+class java1{
+	public static void main(String args[]){
+		int i=5;
+		int d = ++i + ++i + ++i + ++i;
+		System.out.println(d);
+	}
+
+	// public static void fib(int n){
+	// 	int temp=1;
+	// 	int a=0,b=1;
+	// 	System.out.print(a+" ");
+	// 	while(temp<n){
+	// 		System.out.print(b+" ");
+	// 		b=b+a;
+	// 		a=b-a;
+	// 		temp++;
+	// 	}
+	// }
+}
